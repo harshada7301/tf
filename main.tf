362cbf71dfdcb3da9b08e755522c64185192a6b5
unknown <harshada.yadav7301@gmail.com>
2024-06-13 16:57:10 +0530

    commit

--- main.tf
+++ main.tf
@@ -19,3 +19,25 @@ resource "aws_iam_user" "user1" {
 resource "aws_iam_user" "user2" {
   name = "one"
 }
+
+resource "aws_iam_group" "group1" {
+ name = "numbers"
+}
+
+resource "aws_iam_user_group_membership" "grpadd" {
+   user = aws_iam_user.user1.name
+   
+   groups = [
+          aws_iam_group.group1.name
+         ]
+}
+resource "aws_iam_user_group_membership" "grpadd1" {
+   user = aws_iam_user.user2.name
+   
+   groups = [
+          aws_iam_group.group1.name
+         ]
+}
+
+
+
