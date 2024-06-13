45d4ec82f5aa6a5a42ff06cd83c0989a79becdfc
unknown <harshada.yadav7301@gmail.com>
2024-06-13 16:24:38 +0530

    commit 1

--- main.tf
+++ main.tf
@@ -9,71 +9,9 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  profile = "devops-tf"
-}
-# create iam user and groups
-resource "aws_iam_user" "demo" {
-  name = "mahesh"
-
+  profile = "harsha"
 }
 
-resource "aws_iam_user" "demo1" {
-  name = "salonie"
+resource "aws_iam_user" "user1" {
+  name = "naruto"
 }
-
-resource "aws_iam_user" "demo2" {
-  name = "ankita"
-}
-
-resource "aws_iam_group" "grp" {
-  name = "team-dev"
-}
-
-resource "aws_iam_user_group_membership" "grpadd" {
-   user = aws_iam_user.demo1.name
-   
-   groups = [
-          aws_iam_group.grp.name
-         ]
-}
-resource "aws_iam_user_group_membership" "grpadd2" {
-   user = aws_iam_user.demo2.name
-
-   groups = [
-          aws_iam_group.grp.name
-         ]
-}
-resource "aws_iam_user_group_membership" "grpadd3" {
-   user = aws_iam_user.demo.name
-   groups = [
-         aws_iam_group.grp.name
-       ]
-}
-
-
-# code for creating s3 bucket
-resource "aws_s3_bucket" "bucket-1" {
-  bucket = "mangesh-baltiwala"
-   
-}
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket =aws_s3_bucket.bucket-1.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-resource "aws_s3_bucket_acl" "achal" {
-  bucket = aws_s3_bucket.bucket-1.id
-  acl = "private"
-}
-
-
-
-
-
-
-
-
-
-
-
