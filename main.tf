aaa3f99f7bfc613a83229def30b4b5a1976a0b82
unknown <harshada.yadav7301@gmail.com>
2024-06-18 14:31:06 +0530

    one

--- main.tf
+++ main.tf
@@ -39,5 +39,34 @@ resource "aws_iam_user_group_membership" "grpadd1" {
          ]
 }
 
+resource "aws_s3_bucket" "bucket" {
+  bucket = "harsha-s3-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket1" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket2" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bucket3" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucket1,
+    aws_s3_bucket_public_access_block.bucket2,
+  ]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
+}
 
 
